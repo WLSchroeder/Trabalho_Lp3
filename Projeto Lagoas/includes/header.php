bb446cb7c41ee151faba0badb621b35ad9a03e7d
Willian Schroeder Lima <willian.lima1408@gmail.com>
2026-06-09 14:05:32 -0300

    Alteração de nomes
    apenas alterações de nomes

--- Projeto Lagoas/includes/header.php
+++ Projeto Lagoas/includes/header.php
@@ -3,18 +3,18 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>PokéCRUD</title>
+  <title>CineRank</title>
   <link rel="stylesheet" href="../assets/style.css" />
 </head>
 <body>
 
 <header class="site-header">
   <div class="header-inner">
-    <a href="../pages/index.php" class="logo">PokéCRUD</a>
+    <a href="../pages/index.php" class="logo">CineRank</a>
 
     <nav class="nav">
-      <a href="../pages/index.php">Meus Pokémons</a>
-      <a href="../pages/pokemon_create.php">+ Novo Pokémon</a>
+      <a href="../pages/index.php">Meus Filmes e Séries</a>
+      <a href="../pages/pokemon_create.php">+ Novo Filme ou Série </a>
     </nav>
 
     <div class="header-user">
